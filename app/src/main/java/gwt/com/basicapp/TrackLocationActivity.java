a5e6f54034bbff6d4bcea563996bfd4da6a81377
eguo <eguo@localhost.localdomain>
2017-12-13 08:06:40 -0500

    updated the moving car

--- app/src/main/java/gwt/com/basicapp/TrackLocationActivity.java
+++ app/src/main/java/gwt/com/basicapp/TrackLocationActivity.java
@@ -1,15 +1,26 @@
 package gwt.com.basicapp;
 
+import android.animation.ValueAnimator;
+import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Build;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -21,191 +32,282 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class TrackLocationActivity extends PermissionControl implements OnMapReadyCallback {
 
-    private GoogleMap googleMap;
-    private Marker myMarker;
-    private boolean isMarkerRotating = false;
+public class TrackLocationActivity extends PermissionControl implements OnMapReadyCallback,
+        GoogleMap.OnInfoWindowClickListener,GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+    private static final int REQUEST_LOCATION = 0;
+    private Location mLastLocation;
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+    // boolean flag to toggle periodic location updates
+    private boolean mRequestingLocationUpdates = false;
+    //private LocationRequest mLocationRequest;
+    private static final String TAG = "";
+    private GoogleMap mMap;
+    private int markerCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_location);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        markerCount=0;
+
+        //Check If Google Services Is Available
+        if (getServicesAvailable()) {
+            // Building the GoogleApi client
+            Toast.makeText(this, "Google Service Is Available!!", Toast.LENGTH_SHORT).show();
+        }
+
+        //Create The MapView Fragment
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
-    private double bearingBetweenLocations(LatLng latLng1,LatLng latLng2) {
+    /**
+     * GOOGLE MAPS AND MAPS OBJECTS
+     *
+     * */
 
-        double PI = 3.14159;
-        double lat1 = latLng1.latitude * PI / 180;
-        double long1 = latLng1.longitude * PI / 180;
-        double lat2 = latLng2.latitude * PI / 180;
-        double long2 = latLng2.longitude * PI / 180;
+    // After Creating the Map Set Initial Location
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
-        double dLon = (long2 - long1);
-
-        double y = Math.sin(dLon) * Math.cos(lat2);
-        double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1)
-                * Math.cos(lat2) * Math.cos(dLon);
-
-        double brng = Math.atan2(y, x);
-
-        brng = Math.toDegrees(brng);
-        brng = (brng + 360) % 360;
-
-        return brng;
-    }
-
-    private void rotateMarker(final Marker marker, final float toRotation) {
-        if(!isMarkerRotating) {
-            final Handler handler = new Handler();
-            final long start = SystemClock.uptimeMillis();
-            final float startRotation = marker.getRotation();
-            final long duration = 5000;
-
-            final Interpolator interpolator = new LinearInterpolator();
-
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    isMarkerRotating = true;
-
-                    long elapsed = SystemClock.uptimeMillis() - start;
-                    float t = interpolator.getInterpolation((float) elapsed / duration);
-
-                    float rot = t * toRotation + (1 - t) * startRotation;
-
-                    marker.setRotation(-rot > 180 ? rot / 2 : rot);
-                    if (t < 1.0) {
-                        // Post again 16ms later.
-                        handler.postDelayed(this, 16);
-                    } else {
-                        isMarkerRotating = false;
-                    }
-                }
-            });
-        }
-    }
-
-    private void show() {
-
-        LatLng[] locs = {
-                new LatLng(18.4560183333, 73.8247583333),
-                new LatLng(18.4537883333, 73.8249266667),
-                new LatLng(18.4547783333, 73.8247633333),
-                new LatLng(18.4534366667, 73.8220883333)
-        };
-
-        for(LatLng lat: locs) {
-            moveCar(lat);
-            try {
-                Thread.sleep(2000);
-            }catch (Exception e){
-
-            }
-        }
-    }
-    private void moveCar(LatLng SomePos) {
-
+        mMap = googleMap;
         checkPermission();
+        //Uncomment To Show Google Location Blue Pointer
+        // mMap.setMyLocationEnabled(true);
+    }
 
-        try {
-            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            googleMap.setMyLocationEnabled(true);
-            googleMap.setTrafficEnabled(false);
-            googleMap.setIndoorEnabled(false);
-            googleMap.setBuildingsEnabled(true);
-            googleMap.getUiSettings().setZoomControlsEnabled(true);
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(SomePos));
-            googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
-                    .target(googleMap.getCameraPosition().target)
-                    .zoom(17)
-                    .bearing(30)
-                    .tilt(45)
-                    .build()));
+    Marker mk = null;
+    // Add A Map Pointer To The MAp
+    public void addMarker(GoogleMap googleMap, double lat, double lon) {
 
-            this.myMarker = googleMap.addMarker(new MarkerOptions()
-                    .position(SomePos)
-                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
-                    .title("Hello world"));
-
-
-            googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                @Override
-                public boolean onMarkerClick(Marker arg0) {
-
-                    final LatLng startPosition = myMarker.getPosition();
-                    final LatLng finalPosition = new LatLng(12.7801569, 77.4148528);
-                    final Handler handler = new Handler();
-                    final long start = SystemClock.uptimeMillis();
-                    final Interpolator interpolator = new AccelerateDecelerateInterpolator();
-                    final float durationInMs = 3000;
-                    final boolean hideMarker = false;
-
-                    handler.post(new Runnable() {
-                        long elapsed;
-                        float t;
-                        float v;
-
-                        @Override
-                        public void run() {
-                            // Calculate progress using interpolator
-                            elapsed = SystemClock.uptimeMillis() - start;
-                            t = elapsed / durationInMs;
-
-                            LatLng currentPosition = new LatLng(
-                                    startPosition.latitude * (1 - t) + finalPosition.latitude * t,
-                                    startPosition.longitude * (1 - t) + finalPosition.longitude * t);
-
-                            myMarker.setPosition(currentPosition);
-
-                            // Repeat till progress is complete.
-                            if (t < 1) {
-                                // Post again 16ms later.
-                                handler.postDelayed(this, 16);
-                            } else {
-                                if (hideMarker) {
-                                    myMarker.setVisible(false);
-                                } else {
-                                    myMarker.setVisible(true);
-                                }
-                            }
-                        }
-                    });
-
-                    return true;
-
-                }
-
-            });
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(markerCount==1){
+            animateMarker(mLastLocation,mk);
         }
 
+        else if (markerCount==0){
+            //Set Custom BitMap for Pointer
+            int height = 80;
+            int width = 45;
+            BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.mipmap.icon_car);
+            Bitmap b = bitmapdraw.getBitmap();
+            Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+            mMap = googleMap;
+
+            LatLng latlong = new LatLng(lat, lon);
+            mk= mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lon))
+                    //.icon(BitmapDescriptorFactory.fromResource(R.drawable.pin3))
+                    .icon(BitmapDescriptorFactory.fromBitmap((smallMarker))));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlong, 16));
+
+            //Set Marker Count to 1 after first marker is created
+            markerCount=1;
+
+            checkPermission();
+            //mMap.setMyLocationEnabled(true);
+            startLocationUpdates();
+        }
+    }
+
+
+    @Override
+    public void onInfoWindowClick (Marker marker){
+        Toast.makeText(this, marker.getTitle(), Toast.LENGTH_LONG).show();
+    }
+
+
+    public boolean getServicesAvailable() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)) {
+
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Cannot Connect To Play Services", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * LOCATION LISTENER EVENTS
+     *
+     * */
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+//        startLocationUpdates();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        getServicesAvailable();
+
+        // Resuming the periodic location updates
+        if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    //Method to display the location on UI
+    private void displayLocation() {
+
+            checkPermission();
+
+            if (mLastLocation != null) {
+                double latitude = mLastLocation.getLatitude();
+                double longitude = mLastLocation.getLongitude();
+                String loc = "" + latitude + " ," + longitude + " ";
+                Toast.makeText(this,loc, Toast.LENGTH_SHORT).show();
+
+                //Add pointer to the map at location
+                addMarker(mMap,latitude,longitude);
+
+
+            } else {
+
+                Toast.makeText(this, "Couldn't get the location. Make sure location is enabled on the device",
+                        Toast.LENGTH_SHORT).show();
+            }
+    }
+
+
+    //Starting the location updates
+    protected void startLocationUpdates() {
+            checkPermission();
+    }
+
+    //Stopping location updates
+    protected void stopLocationUpdates() {
+    }
+
+    /**
+     * Google api callback methods
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
+                + result.getErrorCode());
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        show();
+    @Override
+    public void onConnected(Bundle arg0) {
+
+        // Once connected with google api, get the location
+        displayLocation();
+
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
+    }
+
+
+    public void onLocationChanged(Location location) {
+        // Assign the new location
+        mLastLocation = location;
+
+        Toast.makeText(getApplicationContext(), "Location changed!",
+                Toast.LENGTH_SHORT).show();
+
+        // Displaying the new location on UI
+        displayLocation();
+    }
+
+
+    public static void animateMarker(final Location destination, final Marker marker) {
+        if (marker != null) {
+            final LatLng startPosition = marker.getPosition();
+            final LatLng endPosition = new LatLng(destination.getLatitude(), destination.getLongitude());
+
+            final float startRotation = marker.getRotation();
+
+            final LatLngInterpolator latLngInterpolator = new LatLngInterpolator.LinearFixed();
+            ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1);
+            valueAnimator.setDuration(1000); // duration 1 second
+            valueAnimator.setInterpolator(new LinearInterpolator());
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override public void onAnimationUpdate(ValueAnimator animation) {
+                    try {
+                        float v = animation.getAnimatedFraction();
+                        LatLng newPosition = latLngInterpolator.interpolate(v, startPosition, endPosition);
+                        marker.setPosition(newPosition);
+                        marker.setRotation(computeRotation(v, startRotation, destination.getBearing()));
+                    } catch (Exception ex) {
+                        // I don't care atm..
+                    }
+                }
+            });
+
+            valueAnimator.start();
+        }
+    }
+
+    private static float computeRotation(float fraction, float start, float end) {
+        float normalizeEnd = end - start; // rotate start to 0
+        float normalizedEndAbs = (normalizeEnd + 360) % 360;
+
+        float direction = (normalizedEndAbs > 180) ? -1 : 1; // -1 = anticlockwise, 1 = clockwise
+        float rotation;
+        if (direction > 0) {
+            rotation = normalizedEndAbs;
+        } else {
+            rotation = normalizedEndAbs - 360;
+        }
+
+        float result = fraction * rotation + start;
+        return (result + 360) % 360;
+    }
+
+}
+
+interface LatLngInterpolator {
+    LatLng interpolate(float fraction, LatLng a, LatLng b);
+
+    class LinearFixed implements LatLngInterpolator {
+        @Override
+        public LatLng interpolate(float fraction, LatLng a, LatLng b) {
+            double lat = (b.latitude - a.latitude) * fraction + a.latitude;
+            double lngDelta = b.longitude - a.longitude;
+            // Take the shortest path across the 180th meridian.
+            if (Math.abs(lngDelta) > 180) {
+                lngDelta -= Math.signum(lngDelta) * 360;
+            }
+            double lng = lngDelta * fraction + a.longitude;
+            return new LatLng(lat, lng);
+        }
     }
 }
